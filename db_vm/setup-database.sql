c58df6a6aaf0e122297c83ae93e99dd527615ebe
Harrison <harrisonbjones2003@gmail.com>
2025-09-02 15:56:53 +1200

    Database CSV read fix

--- db_vm/setup-database.sql
+++ db_vm/setup-database.sql
@@ -1,9 +1,16 @@
-CREATE TABLE fib (
+USE fvision;
+
+-- Create table if it doesn't exist
+CREATE TABLE IF NOT EXISTS fib (
     id INT PRIMARY KEY,
     value NUMERIC(50,0)
 );
 
-LOAD DATA LOCAL INFILE 'fib.csv'
+-- Refresh table data
+TRUNCATE TABLE fib;
+
+-- Load CSV
+LOAD DATA LOCAL INFILE '/vagrant/fib.csv'
 INTO TABLE fib
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
